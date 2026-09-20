4e98f316653b3f734a52dddd141c1d014771cb02
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-09-19 19:15:44 -0500

    Build starter template

---
+++ desktopApp/build.gradle.kts
@@ -0,0 +1,23 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+dependencies {
+    implementation(project(":shared"))
+    implementation(compose.desktop.currentOs)
+}
+
+compose.desktop {
+    application {
+        mainClass = "com.hereliesaz.templatempp.desktop.MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "HereLiesAzTemplate"
+            packageVersion = "1.0.0"
+        }
+    }
+}
